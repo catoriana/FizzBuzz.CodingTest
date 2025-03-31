f0a39343f568f3baa7a7daa2f396f4b6080703d1
catoriana <catoriana@hotmail.it>
2025-03-31 10:50:34 +0100

    Added Dependency Injection into the Program.cs class to complete SOLID best practices.

--- FizzBuzz.CodingTest/Program.cs
+++ FizzBuzz.CodingTest/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace FizzBuzz.CodingTest;
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FizzBuzz.CodingTest;
 public class Program
 {
     static void Main(string[] args)
     {
-        var _processor = new FizzBuzzProcessor();
+        //No need to inject the evaulators here, the processor will take care of that via reflection
+        //but normally you would inject the evaluators here
+        var serviceProvider = new ServiceCollection()
+        .AddSingleton<IFizzBuzzProcessor, FizzBuzzProcessor>()
+        .BuildServiceProvider();
+
+
+        // Get an instance of the service from the DI container
+        var _processor = serviceProvider.GetRequiredService<IFizzBuzzProcessor>();
+
         Console.WriteLine(_processor.Process(1, 100));
     }
 }
