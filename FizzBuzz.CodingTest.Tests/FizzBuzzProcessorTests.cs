00e1d0ed7a47d6cf0ad637e41df8348c85023d60
catoriana <catoriana@hotmail.it>
2025-03-31 10:36:04 +0100

    Add Interface and Class for FizzBuzz Processor Add Unit Tests - Failing

---
+++ FizzBuzz.CodingTest.Tests/FizzBuzzProcessorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.CodingTest.Tests;
+
+public class FizzBuzzProcessorTests
+{
+    [Fact]
+    public void GetEvaluators_WhenCalled_Returns_ListOfFizzBuzzEvaluators()
+    {
+        Assert.Fail();
+    }
+
+    [Fact]
+    public void Process_WhenCalled_Returns_ExpectedResult()
+    {
+        Assert.Fail();
+    }
+}
---
+++ FizzBuzz.CodingTest/FizzBuzzProcessor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.CodingTest;
+
+public class FizzBuzzProcessor : IFizzBuzzProcessor
+{
+    public List<IFizzBuzzEvaluator> GetEvaluators()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Process(int startRange, int endRange)
+    {
+        throw new NotImplementedException();
+    }
+}
